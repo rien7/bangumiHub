80876e17753089e13af40b5cfca6c83b29fcec2c
RieN7 <zrien7@gmail.com>
2023-10-18 20:22:39 +0800

    fix(fark): add dark listener

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { SESSION_STRING } from './utils/client'
 import './utils/worker'
 
@@ -8,7 +8,15 @@ const router = useRouter()
 if (!SESSION_STRING)
   router.push('/login')
 
-const datkMode = ref(window.matchMedia('(prefers-color-scheme: dark)').matches)
+const datkMode = ref()
+
+onMounted(() => {
+  datkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches
+  window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+      datkMode.value = event.matches
+    })
+})
 </script>
 
 <template>
